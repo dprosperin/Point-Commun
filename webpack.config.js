035da430680cac99f27eb76cc5377fe4bb46669d
cubx2015 <davidprosperin@gmail.com>
2018-11-01 17:39:53 +0100

    Adding the logo Point Commun

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 var HTMLPlugin =  require("html-webpack-plugin");
-
 module.exports = {
     plugins: [
         new HTMLPlugin({
-            title: 'Point Commun'
+            title: 'Point Commun',
+            filename: 'index.html',
+            template: 'src/index.html',
+            favicon: 'src/images/logoPointCommun.png',
+            meta: {
+                viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+                'theme-color': '#4285f4'
+            },
+            hash: true,
+            showErrors: true,
+            
         })
     ]
 } 
