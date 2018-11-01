38a3d83222f373d128244d7435ed020e166de18c
cubx2015 <davidprosperin@gmail.com>
2018-11-01 23:16:07 +0100

    Adding carousel with Flickity

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello");
+import $  from 'jquery';
+import 'flickity/css/flickity.css';
+import './css/app.css';
+import Flickity from 'flickity';
+
+const flky = new Flickity('.carousel', {
+    pageDots: false,
+    prevNextButtons: false,
+    wrapAround: true,
+    autoPlay: true,
+    imagesLoaded: false, 
+    percentPosition: false
+})
